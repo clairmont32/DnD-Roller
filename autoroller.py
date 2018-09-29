52fdacab4677482d520bb77cb7b62ec79146bf99
Matthew Clairmont <>
2018-09-29 15:22:13 -0400

    asserted stat type is int

--- autoroller.py
+++ autoroller.py
@@ -19,14 +19,14 @@ def add_character_stats():
 
     # prompt for character stats
     name = str(input('Enter character name: \n'))
-    armor_class = input('Enter armor class: \n')
-    initiative = input('Enter initiative: \n')
-    hit_points = input('Enter HP: \n')
-    attack_hit = input('Enter attack hit: \n')
-    damage_dice = input('Enter damage dice number: \n')
-    damage_bonus = input('Enter damage bonus: \n')
-    cure = input('Enter cure: \n')
-    num_attacks = input('Enter number of attacks: \n')
+    armor_class = int(input('Enter armor class: \n'))
+    initiative = int(input('Enter initiative: \n'))
+    hit_points = int(input('Enter HP: \n'))
+    attack_hit = int(input('Enter attack hit: \n'))
+    damage_dice = int(input('Enter damage dice number: \n'))
+    damage_bonus = int(input('Enter damage bonus: \n'))
+    cure = int(input('Enter cure: \n'))
+    num_attacks = int(input('Enter number of attacks: \n'))
 
     # add to json
     characters[name] = {
@@ -48,6 +48,42 @@ def add_character_stats():
     return
 
 
+# prompt for and add monster stats to json file
+def add_monster_stats():
+    # read current monster stats in json dictionary for
+    with open('monsters.json', 'r') as file:
+        monsters = json.load(file)
+
+    # prompt for monster stats
+    name = str(input('Enter monster name: \n'))
+    armor_class = int(input('Enter armor class: \n'))
+    initiative = int(input('Enter initiative: \n'))
+    hit_points = int(input('Enter HP: \n'))
+    attack_hit = int(input('Enter attack hit: \n'))
+    damage_dice = int(input('Enter damage dice number: \n'))
+    damage_bonus = int(input('Enter damage bonus: \n'))
+    cure = int(input('Enter cure: \n'))
+    num_attacks = int(input('Enter number of attacks: \n'))
+
+    # add to json
+    monsters[name] = {
+        "armor_class": armor_class,
+        "initiative": initiative,
+        "hit_points": hit_points,
+        "attack_hit": attack_hit,
+        "damage_dice": damage_dice,
+        "damage_bonus": damage_bonus,
+        "cure": cure,
+        "num_attacks": num_attacks
+    }
+
+    # delete the old data and write the new
+    with open('monsters.json', 'r+') as file:
+        file.truncate(0)
+        file.write(json.dumps(monsters))
+
+    return
+
 # roll a die with a given amount of sides
 def roll_die(sides):
     result = random.randint(1, sides)
@@ -58,4 +94,3 @@ def roll_die(sides):
  #   return player[attackHit]
 
 
-add_character_stats()
