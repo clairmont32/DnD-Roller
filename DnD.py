05541ddbeefcd167d5349c339676950182dcac90
Matthew <6239004+clairmont32@users.noreply.github.com>
2018-10-02 14:31:37 -0400

    Add monster class

--- DnD.py
+++ DnD.py
@@ -11,9 +11,6 @@ class Characters:
 		if os.path.exists('characters.json') is False:
 			exit(print('Could not find characters.json in '.format(cwd)))
 
-		if os.path.exists('monsters.json') is False:
-			exit(print('Could not find monsters.json in '.format(cwd)))
-
 		with open('Characters.json', 'r') as characters_file:
 			self.characters = json.load(characters_file)
 
@@ -99,3 +96,90 @@ class Characters:
 			characters_file.truncate(0)
 			characters_file.write(json.dumps(self.characters))
 
+# means of interacting with monsters in monsters.json
+class Monsters:
+
+	def __init__(self):
+		cwd = os.getcwd()
+
+		if os.path.exists('monsters.json') is False:
+			exit(print('Could not find monsters.json in '.format(cwd)))
+
+		with open('monsters.json', 'r') as monsters_file:
+			self.monsters = json.load(monsters_file)
+
+	# creates an empty monster in monsters.json
+	def create_monster(self, name):
+		self.monsters[name] = {}
+
+		with open('monsters.json', 'w') as monsters_file:
+			monsters_file.truncate(0)
+			monsters_file.write(json.dumps(self.monsters))
+
+		return
+
+	# removes a monster from monsters.json
+	def remove_monster(self, name):
+		try:
+			del self.monsters[name]
+			with open('monsters.json', 'w') as monsters_file:
+				monsters_file.truncate(0)
+				monsters_file.write(json.dumps(self.monsters))
+
+		except KeyError:
+			print('{!s} does not have a monster profile saved!'.format(name))
+
+		return
+
+	# lists current monster names
+	def list_monsters(self):
+		names = [name for name in self.monsters.keys()]
+		for name in names:
+			print(name)
+
+	# prompt for monster stats
+	def add_stats(self):
+		name = str(input('Enter monster name: \n'))
+		print('Enter numeric values for the below stats- ')
+		while True:
+			try:
+				armor_class = int(input('Enter armor class: \n'))
+				initiative = int(input('Enter initiative: \n'))
+				hit_points = int(input('Enter HP: \n'))
+				attack_hit = int(input('Enter attack hit: \n'))
+				damage_dice = int(input('Damage die roll (1dX): \n'))
+				num_attacks = int(input('Enter number of attacks: \n'))
+
+				break
+			except ValueError:
+				print('You didn\'t enter a number!')
+
+		# add to json
+		self.monsters[name] = {"armorClass": armor_class, "initiative": initiative, "hitPoints": hit_points,
+		                         "attackHit": attack_hit, "damageDice": damage_dice, "numAttacks": num_attacks}
+
+		with open('monsters.json', 'w') as monsters_file:
+			monsters_file.truncate(0)
+			monsters_file.write(json.dumps(self.monsters))
+
+	# lists stats for a given monster
+	def list_stats(self, name):
+		try:
+			for stat in self.monsters[name].keys():
+				print(stat)
+		except KeyError:
+			print('{!s} does not have a monster profile saved!'.format(name))
+
+		return
+
+	# updates a current stat
+	def change_stat(self, name, stat, new_stat):
+		try:
+			self.monsters[name].update({stat: new_stat})
+		except KeyError as ker:
+			print('Could not update {!s}'.format(new_stat))
+			print(ker)
+
+		with open('monsters.json', 'w') as monsters_file:
+			monsters_file.truncate(0)
+			monsters_file.write(json.dumps(self.monsters))
